412704d27de7e470e80548f39ad54897e411b54e
cristianoflima <cristianolima_udi@hotmail.com>
2018-04-11 22:40:36 +0100

    Adicionado bootstrap, iniciado front-end para tela de cadastro.

--- src/main/java/br/com/cflima/livrariaObpc/LivrariaObpcApplication.java
+++ src/main/java/br/com/cflima/livrariaObpc/LivrariaObpcApplication.java
@@ -4,6 +4,8 @@ package br.com.cflima.livrariaObpc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
 public class LivrariaObpcApplication {
@@ -11,4 +13,5 @@ public class LivrariaObpcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LivrariaObpcApplication.class, args);
 	}
+	
 }
